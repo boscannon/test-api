76a51e0b025a8c64b91a3173d3b1d8567865455c
cannon <cannon>
2022-09-06 06:03:57 +0800

    '11'

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category as CurdModel;
 
 class CategoryController extends Controller
 {
-    //
+	public static $rules = [
+		'name' => ['required', 'string', 'max:50']
+	];
+	public static $messages = [];
+	public static $attributes = [];
+
+	public function index(Request $request)
+	{
+		$rules = [            
+			'query' => ['nullable', 'string'],
+			'page' => ['nullable', 'numeric'],
+			'per_page' => ['nullable', 'numeric'],       
+		];
+
+		$validatedData = $request->validate($rules, self::$messages, self::$attributes);
+
+		return CurdModel::search($validatedData['query'] ?? '')
+			->paginate($validatedData['per_page'] ?? 15);
+	}
+
+	public function store(Request $request)
+	{
+		$validatedData = $request->validate(self::$rules, self::$messages, self::$attributes);
+		try {
+			CurdModel::create($validatedData);
+			return Response()->json(['message' => __('Store Success')]);
+		} catch(\Exception $e) {
+			return Response()->json(['message' => $e->getMessage()], 422);
+		}
+	}
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
+
+    protected $fillable = [
+    	'name'
+    ];
+
+    protected $casts = [
+    	'name' => 'string'
+    ];
 
     public function Products()
     {
